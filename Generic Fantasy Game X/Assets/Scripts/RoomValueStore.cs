f803c60e1b01d06fed5684a8d9f78080cb8e0ddb
Lachlan <lachlan.d.wood@student.uts.edu.au>
2018-04-14 16:59:32 +1000

    Procedural Dungeon Generation
     - Huge Milestone
     - Establishment of Dungeon Generation
     - No overlapping rooms as part of the generation
     - Rooms are stitched together seemlessly
     - Bakes the navmesh as part of the generation of the level

---
+++ Generic Fantasy Game X/Assets/Scripts/RoomValueStore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomValueStore : MonoBehaviour {
+
+	public float xSize = 1.0f;
+	public float zSize = 1.0f;
+	public List<GameObject> roomInternodes;
+	public List<GameObject> floorPlanObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void removeNodeAvailability(GameObject nodeToRemove) {
+		roomInternodes.Remove (nodeToRemove);
+		//DestroyImmediate (nodeToRemove, false);
+		//Destroy (nodeToRemove);
+		nodeToRemove.SetActive (false);
+	}
+
+	public void removeNodeAvailability(int nodeToRemoveIndex) {
+		GameObject nodeToRemove = roomInternodes [nodeToRemoveIndex];
+		roomInternodes.Remove (nodeToRemove);
+		//DestroyImmediate (nodeToRemove, false);
+		//Destroy (nodeToRemove);
+		nodeToRemove.SetActive (false);
+	}
+}
