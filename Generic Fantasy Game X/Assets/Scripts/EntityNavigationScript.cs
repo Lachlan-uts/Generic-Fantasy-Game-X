04d85570ae6648bc5f92276a3f2ae41c0d649bd6
Lachlan-uts <lachlan.d.wood@student.uts.edu.au>
2018-04-17 17:19:30 +1000

    Successful test of navmesh guidance
    This work and proves it's possible to guide entities using the navmesh system.

---
+++ Generic Fantasy Game X/Assets/Scripts/EntityNavigationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EntityNavigationScript : MonoBehaviour {
+
+	[SerializeField]
+	private Transform goal;
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		//agent.destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = goal.position;
+	}
+}
