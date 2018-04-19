0a1e3d2b3ac40eed2b3966ed8a6206853e96d065
DESKTOP-IU5PEHD\ckhwong008 <12617092@student.uts.edu.au>
2018-04-19 22:00:13 +1000

    Added right-click and drag to move camera

---
+++ Generic Fantasy Game X/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float dragSpeed = 1;
+
+    private Vector3 dragOrigin;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+        transform.Translate(move, Space.World);
+    }
+}
