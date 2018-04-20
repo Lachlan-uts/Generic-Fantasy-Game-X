393d9671cd2ede0669b3672a288593536ab15da6
Rowena Budiharto <11981673@student.uts.edu.au>
2018-04-20 22:08:54 +1000

    Single Player Information Attached
    Single Player Information Attached, ready drafted class script for multiple characters, draft UI placement.

---
+++ Generic Fantasy Game X/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+	void Start()
+	{
+		PlayerInformation firstPlayer = new PlayerInformation ();
+
+		firstPlayer.Name = "First Hero";
+		firstPlayer.Exp = 0;
+		firstPlayer.Level = (firstPlayer.Exp / 100);
+		firstPlayer.HP = 100;
+		firstPlayer.Damage = 10;
+		firstPlayer.isActive = true;
+
+		Debug.Log ("First Character: " 
+			+ firstPlayer.Name + " " 
+			+ firstPlayer.Exp + " " 
+			+ firstPlayer.Level + " " 
+			+ firstPlayer.HP + " " 
+			+ firstPlayer.Damage + " " 
+			+ firstPlayer.isActive);
+	}
+
+}
---
+++ Generic Fantasy Game X/Assets/Scripts/PlayerInformation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInformation : MonoBehaviour {
+	public string Name { get; set; }
+	public int Level { get; set; }
+	public int Exp { get; set; }
+	public int HP { get; set; }
+	public int Damage { get; set; }
+	public bool isActive { get; set; }
+
+	public PlayerInformation()
+	{
+		this.Name = "No Name";
+		this.Exp = 0;
+		this.Level = ( Exp / 100);
+		this.HP = 100;
+		this.Damage = 10;
+		isActive = false;
+	}
+}
+
+	public class Slot1 : PlayerInformation
+	{
+	public Slot1()
+	{
+		Name = "First Hero";
+		Exp = 0;
+		Level = ( Exp / 100 );
+		HP = 100;
+		Damage = 10;
+		isActive = true;
+
+		Debug.Log ("First Character: " + Name + " " + Exp + " " + Level + " " + HP + " " + Damage + " " + isActive);
+	}
+	}
+
+
