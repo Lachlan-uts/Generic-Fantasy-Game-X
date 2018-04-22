0f122cf4b4754bd704b277d028a75bde155d351c
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-04-22 16:37:28 +1000

    Enemy prep + better movement system.
    Reduced the error rate by allowing the system to not move when dentination doesn't exist properly. Also merged master back into this to allow for merge compatability.

--- Generic Fantasy Game X/Assets/Scripts/EntityNavigationScript.cs
+++ Generic Fantasy Game X/Assets/Scripts/EntityNavigationScript.cs
@@ -18,6 +18,7 @@ public class EntityNavigationScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		agent.destination = goal.position;
+		if (goal != null)
+			agent.destination = goal.position;
 	}
 }
