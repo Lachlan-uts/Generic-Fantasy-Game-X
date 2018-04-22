548362b31b6dd13ca01eeeea41a00979909f20cf
DESKTOP-IU5PEHD\ckhwong008 <12617092@student.uts.edu.au>
2018-04-23 00:12:58 +1000

    Equipped hero with melee weapons. Moved Weapon Prefabs from Assets folder to prefab folder

--- Generic Fantasy Game X/Assets/Scripts/WeaponEquip.cs
+++ Generic Fantasy Game X/Assets/Scripts/WeaponEquip.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class WeaponEquip : MonoBehaviour {
 
+    GameObject currentWeapon;
+    bool swordActive, axeActive, maceActive;
+
     [SerializeField]
-    GameObject weapon;
+    List<GameObject> weapons;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        currentWeapon = weapons[0];
+        swordActive = true;
+        axeActive = false;
+        maceActive = false;
+        EquipSword();
+        EquipAxe();
+        EquipMace();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
+    }
+
+    void EquipSword()
+    {
+        if (swordActive == true)
+        {
+            weapons[0].SetActive(true);
+        }
+        else weapons[0].SetActive(false);
+    }
+
+    void EquipAxe()
+    {
+        if (axeActive == true)
+        {
+            weapons[1].SetActive(true);
+        }
+        else weapons[1].SetActive(false);
+    }
+
+    void EquipMace()
+    {
+        if (maceActive == true)
+        {
+            weapons[2].SetActive(true);
+        }
+        else weapons[2].SetActive(false);
+    }
 }
