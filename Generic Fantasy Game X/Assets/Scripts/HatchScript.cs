f4955d8d3b6d7ecb858759621f57e53b56b18543
Mordakai95Uni <William.G.Palmer@student.uts.edu.au>
2018-04-27 15:15:26 +1000

    HatchWork
    Started a quality of life improvement so that enemies don't try to swarm heroes whilst the dungeon is still being generated.

---
+++ Generic Fantasy Game X/Assets/Scripts/HatchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatchScript : MonoBehaviour {
+
+	// private variables
+	private float hatchRange = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ExitLevel() {
+		GameObject[] heroes = GameObject.FindGameObjectsWithTag ("Hero");
+		bool canLeave = true;
+		foreach (GameObject hero in heroes) {
+			if (Vector3.Distance (hero.transform.position, gameObject.transform.position) > hatchRange) {
+				canLeave = false;
+			}
+		}
+
+		if (canLeave) {
+			Debug.Log ("Level Complete!");
+		}
+	}
+}
