b97b3b18d86b6ee674462569539cee5c90729ec3
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-04-30 03:53:41 +1000

    Better item equip system
    You now actually hold items as actual items in your inventory. Also when you drop them they should become physical entities.

---
+++ Generic Fantasy Game X/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+
+	private Rigidbody weaponRGBDY;
+	private Mesh weaponM;
+	private MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		weaponRGBDY = GetComponentInChildren<Rigidbody> ();
+		rend = GetComponentInChildren<MeshRenderer> ();
+		SetState (this.transform.parent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetState(Transform owner = null) {
+		if (owner == null) {
+			SetUnequiped ();
+		} else if (this.gameObject.transform.parent.name == "EquipedItems") {
+			SetEquiped ();
+		} else {
+			SetInInventory ();
+		}
+	}
+
+	private void SetEquiped() {
+		weaponRGBDY.useGravity = false;
+		weaponRGBDY.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+	}
+
+	private void SetUnequiped() {
+		weaponRGBDY.useGravity = true;
+		weaponRGBDY.constraints = RigidbodyConstraints.None;
+	}
+
+	private void SetInInventory() {
+		rend.enabled = false;
+	}
+}
