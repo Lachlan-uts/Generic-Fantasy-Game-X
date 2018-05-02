af3606e97be47fa1af656f8fd88801742ee1d576
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-05-02 14:12:58 +1000

    NerdSnipped
    I know this started out as combat interactions as its goal, however I have well and truely nerd sniped myself. I've now got a semi functional physics system for weapons which allow them to return to their point of origin.

--- Generic Fantasy Game X/Assets/Scripts/WeaponScript.cs
+++ Generic Fantasy Game X/Assets/Scripts/WeaponScript.cs
@@ -11,14 +11,22 @@ public class WeaponScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		weaponRGBDY = GetComponentInChildren<Rigidbody> ();
-		rend = GetComponentInChildren<MeshRenderer> ();
+		weaponRGBDY = GetComponent<Rigidbody> ();
+		rend = GetComponent<MeshRenderer> ();
 		SetState (this.transform.parent);
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		StartCoroutine ("ReturnToOriginPosition");
+		StartCoroutine ("ReturnToOriginRotation");
+		if (Input.GetButtonDown("Fire1")) {
+			StartCoroutine ("AttackTarget");
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log (collision.collider.tag);
 	}
 
 	private void SetState(Transform owner = null) {
@@ -30,9 +38,11 @@ public class WeaponScript : MonoBehaviour {
 			SetInInventory ();
 		}
 	}
+		
 
 	private void SetEquiped() {
 		weaponRGBDY.useGravity = false;
+		//weaponRGBDY.isKinematic = true;
 		weaponRGBDY.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
 	}
 
@@ -43,5 +53,35 @@ public class WeaponScript : MonoBehaviour {
 
 	private void SetInInventory() {
 		rend.enabled = false;
+	}
+
+	private IEnumerator ReturnToOriginPosition() {
+		Debug.Log ("where it currently is " + transform.localPosition);
+		if (transform.localPosition != Vector3.zero) {
+			Debug.Log ("gotta get back");
+			if (weaponRGBDY.velocity.magnitude <= 0.1) {
+				yield return null;
+			}
+			Debug.Log ("added force" + (Vector3.zero - transform.localPosition));
+			weaponRGBDY.AddRelativeForce ((Vector3.zero - transform.localPosition), ForceMode.Force);
+			if (weaponRGBDY.velocity.magnitude >= 4) {
+				weaponRGBDY.velocity = Vector3.zero;
+				//weaponRGBDY.AddForce ((Vector3.zero - weaponRGBDY.velocity.normalized), ForceMode.Force);
+				//weaponRGBDY.drag = weaponRGBDY.drag
+			}
+			//weaponRGBDY.AddTorque ((Vector3.zero - transform.localPosition), ForceMode.Force);
+		}
+		yield return null;
+	}
+
+	private IEnumerator ReturnToOriginRotation() {
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0,0,0), Time.deltaTime * 5.0f);
+		yield return null;
+	}
+
+	private IEnumerator AttackTarget() {
+		weaponRGBDY.AddForce (Vector3.forward, ForceMode.Impulse);
+		weaponRGBDY.AddRelativeTorque (Vector3.right * 2);
+		yield return null;
 	}
 }
