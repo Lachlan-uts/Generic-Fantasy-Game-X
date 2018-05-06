1d5f59fce0e918a7fe66f836d64d2cf9cfd81c1d
William <William.G.Palmer@student.uts.edu.au>
2018-05-06 15:20:45 +1000

    Dropping Items which can be picked up in the future
    For the moment, the drop manager can be used to create loot which can be picked up in the future. I say in the future, because we will need an inventory system and way for entities to pick up loot before we can test picking it up properly.

---
+++ Generic Fantasy Game X/Assets/Scripts/DropManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropManagerScript : MonoBehaviour {
+
+	// serialized private variables
+	[SerializeField]
+	private List<GameObject> itemTemplates;
+	//[SerializeField]
+	//private List<GameObject> rareItemTemplates;
+	[SerializeField]
+	private GameObject lootHolder;
+
+	// private variables
+	private int floorNumber;
+
+	// Use this for initialization
+	void Start () {
+		GameObject levelGenerator;
+		if ((levelGenerator = GameObject.Find ("LevelGenerator")) != null) {
+			floorNumber = levelGenerator.GetComponent<LevelGenerationScript> ().floorNumber;
+		} else {
+			floorNumber = 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.L)) {
+			generateLoot (this.gameObject.transform);
+		}
+	}
+
+	public void generateLoot(Transform lootPosition) {
+		GameObject newItem = Instantiate (itemTemplates [Random.Range (0, itemTemplates.Count)]) as GameObject;
+		// create code to dynamically generate the item's properties
+
+
+
+		GameObject newLootHolder = Instantiate (lootHolder, lootPosition.position, Quaternion.identity) as GameObject;
+		newItem.transform.SetParent (newLootHolder.transform);
+		newLootHolder.GetComponent<DropScript> ().lootItem = newItem;
+	}
+}
---
+++ Generic Fantasy Game X/Assets/Scripts/PotionUsageScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionUsageScript : MonoBehaviour {
+
+	// private variables
+	public int fluidAmount { get; private set; }
+	public int fluidLimit { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		fluidAmount = 5; // Placeholder
+		fluidLimit = 15; // Placeholder
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int Quaff(int amountNeeded) { // 'Quaff' an amount needed, so can conserve potions. Integer returned is the amount healed
+		int fluidConsumed = 0;
+		if (amountNeeded > fluidAmount) {
+			fluidConsumed = fluidAmount;
+			fluidAmount = 0;
+		} else {
+			fluidConsumed = amountNeeded;
+			fluidAmount -= amountNeeded;
+		}
+
+		return fluidConsumed;
+	}
+
+	public int Refill(int amountRefilled) { // 'Refills' the amount poured in. Integer returned is the amount leftover.
+		int fluidRefilled = 0;
+		int fluidLeft = 0;
+		if (amountRefilled > (fluidLimit - fluidAmount)) {
+			fluidRefilled = fluidLimit - fluidAmount;
+			fluidAmount = fluidLimit;
+			fluidLeft = amountRefilled - fluidRefilled;
+		} else {
+			fluidRefilled = amountRefilled;
+			fluidAmount += fluidRefilled;
+			fluidLeft = 0;
+		}
+
+		return fluidLeft;
+	}
+
+	public void Refill() { // The simpler version, which returns nothing
+		fluidAmount = fluidLimit;
+	}
+
+}
