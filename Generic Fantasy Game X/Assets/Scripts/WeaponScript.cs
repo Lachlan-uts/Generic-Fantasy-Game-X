29a6791a782392b2723a0deb510c6b724184068c
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-05-06 17:57:21 +1000

    Won't attack self
    Fixed the attack function so an entity can't attack itself.

--- Generic Fantasy Game X/Assets/Scripts/WeaponScript.cs
+++ Generic Fantasy Game X/Assets/Scripts/WeaponScript.cs
@@ -8,25 +8,32 @@ public class WeaponScript : MonoBehaviour {
 	private Rigidbody weaponRGBDY;
 	private Mesh weaponM;
 	private MeshRenderer rend;
+	private GameObject equiper;
 
 	// Use this for initialization
 	void Start () {
 		weaponRGBDY = GetComponent<Rigidbody> ();
 		rend = GetComponent<MeshRenderer> ();
+		equiper = null;
 		SetState (this.transform.parent);
-
 	}
 
 	void Update () {
 		StartCoroutine ("ReturnToOriginPosition");
 		StartCoroutine ("ReturnToOriginRotation");
-		if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetKeyDown(KeyCode.Mouse2)) {
 			StartCoroutine ("AttackTarget");
 		}
 	}
 
 	void OnCollisionEnter(Collision collision) {
-		Debug.Log (collision.collider.tag);
+		if (collision.collider.gameObject == equiper) {
+			Debug.Log ("can't hit myself");
+			return;
+		}
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Units")) {
+			Debug.Log ("Trigger the attack/kill!");
+		}
 	}
 
 	private void SetState(Transform owner = null) {
@@ -41,6 +48,7 @@ public class WeaponScript : MonoBehaviour {
 		
 
 	private void SetEquiped() {
+		equiper = GetComponentInParent<EntityTargetScript> ().gameObject;
 		weaponRGBDY.useGravity = false;
 		//weaponRGBDY.isKinematic = true;
 		weaponRGBDY.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
@@ -56,32 +64,32 @@ public class WeaponScript : MonoBehaviour {
 	}
 
 	private IEnumerator ReturnToOriginPosition() {
-		Debug.Log ("where it currently is " + transform.localPosition);
+		//Debug.Log ("where it currently is " + transform.localPosition);
 		if (transform.localPosition != Vector3.zero) {
-			Debug.Log ("gotta get back");
+			//Debug.Log ("gotta get back");
 			if (weaponRGBDY.velocity.magnitude <= 0.1) {
 				yield return null;
 			}
-			Debug.Log ("added force" + (Vector3.zero - transform.localPosition));
+			//Debug.Log ("added force" + (Vector3.zero - transform.localPosition));
 			weaponRGBDY.AddRelativeForce ((Vector3.zero - transform.localPosition), ForceMode.Force);
-			if (weaponRGBDY.velocity.magnitude >= 4) {
-				weaponRGBDY.velocity = Vector3.zero;
-				//weaponRGBDY.AddForce ((Vector3.zero - weaponRGBDY.velocity.normalized), ForceMode.Force);
-				//weaponRGBDY.drag = weaponRGBDY.drag
-			}
+//			if (weaponRGBDY.velocity.magnitude >= 4) {
+//				weaponRGBDY.velocity = Vector3.zero;
+//				weaponRGBDY.AddForce ((Vector3.zero - weaponRGBDY.velocity.normalized), ForceMode.Force);
+//				weaponRGBDY.drag = weaponRGBDY.drag
+//			}
 			//weaponRGBDY.AddTorque ((Vector3.zero - transform.localPosition), ForceMode.Force);
 		}
 		yield return null;
 	}
 
 	private IEnumerator ReturnToOriginRotation() {
-		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0,0,0), Time.deltaTime * 5.0f);
+		//transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0,0,0), Time.deltaTime * 5.0f);
 		yield return null;
 	}
 
 	private IEnumerator AttackTarget() {
-		weaponRGBDY.AddForce (Vector3.forward, ForceMode.Impulse);
-		weaponRGBDY.AddRelativeTorque (Vector3.right * 2);
+		weaponRGBDY.AddRelativeForce (Vector3.forward*16, ForceMode.Force);
+		//weaponRGBDY.AddRelativeTorque (Vector3.up * 4);
 		yield return null;
 	}
 }
