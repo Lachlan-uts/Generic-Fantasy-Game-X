31a0e5da8b6417f0278092dd851be3683ef6ccb4
Chris <12617092@student.uts.edu.au>
2018-05-10 19:16:46 +1000

    Setup for Mission Objectives to Complete Level (Space Key to activate completion)

--- Generic Fantasy Game X/Assets/Scripts/HatchScript.cs
+++ Generic Fantasy Game X/Assets/Scripts/HatchScript.cs
@@ -7,9 +7,42 @@ public class HatchScript : MonoBehaviour {
 	// private variables
 	private float hatchRange = 6.0f;
 
+    private float missionObjective;
+    private bool enemyCondition = false;
+    private bool obtainKeyCondition = false;
+
+    private int enemyKills;
+
 	// Use this for initialization
 	void Start () {
-		
+        missionObjective = Random.Range(0, 2);
+
+        //For the moment, we'll just make the objective to kill a certain number of enemies
+        missionObjective = 0;
+
+        //For Later when creating different objectives
+        if (missionObjective == 0)
+        {
+            enemyCondition = true;
+        }
+        else if (missionObjective == 1)
+        {
+            obtainKeyCondition = true;
+        }
+
+        if (enemyCondition)
+        {
+            SetEnemyObjective(1);
+        }
+
+        if (obtainKeyCondition)
+        {
+            SetKeyObjective();
+        }
+
+        Debug.Log(missionObjective);
+        Debug.Log(enemyCondition);
+        Debug.Log(obtainKeyCondition);
 	}
 	
 	// Update is called once per frame
@@ -17,17 +50,83 @@ public class HatchScript : MonoBehaviour {
 		
 	}
 
-	public void ExitLevel() {
-		GameObject[] heroes = GameObject.FindGameObjectsWithTag ("Hero");
-		bool canLeave = true;
-		foreach (GameObject hero in heroes) {
-			if (Vector3.Distance (hero.transform.position, gameObject.transform.position) > hatchRange) {
-				canLeave = false;
-			}
-		}
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Hero")
+        {
+            //Debug.Log("Hero has reached Exit");
+            if (CanLeave())
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Debug.Log("Level Complete");
+                    ExitLevel();
+                }
+            }
+            else
+            {
+                Debug.Log("Mission Incomplete");
+            }
+            
+        }
+    }
 
-		if (canLeave) {
-			Debug.Log ("Level Complete!");
-		}
-	}
+    private bool CanLeave()
+    {
+        //Check Objective Completion
+        bool canLeave = true;
+        if (CompleteKeyObjective() || CompleteEnemyObjective())
+        {
+            GameObject[] heroes = GameObject.FindGameObjectsWithTag("Hero");
+            foreach (GameObject hero in heroes)
+            {
+                if (Vector3.Distance(hero.transform.position, gameObject.transform.position) > hatchRange)
+                {
+                    canLeave = false;
+                }
+            }
+        }
+        else
+        {
+            canLeave = false;
+        }
+        return canLeave;
+    }
+
+    public void ExitLevel()
+    {
+        GameObject[] heroes = GameObject.FindGameObjectsWithTag("Hero");
+        foreach (GameObject hero in heroes)
+        {
+            DontDestroyOnLoad(hero);
+        }
+
+        //Create End Screen
+    }
+
+    void CheckObjective()
+    {
+
+    }
+
+    private void SetEnemyObjective(int i)
+    {
+        //Kill No. Of Enemies based on i value
+    }
+
+    private bool CompleteEnemyObjective()
+    {
+        return true;
+    }
+
+    private void SetKeyObjective()
+    {
+        //Deliver Key to Exit Room
+        //Check if Team has Key
+    }
+
+    private bool CompleteKeyObjective()
+    {
+        return true;
+    }
 }
