a6d2280945abbf4fbbe19ab06d08fe895cead1c1
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-05-12 13:47:56 +1000

    Mouse edge of screen movement
    Also have that working as traditionally expected.

--- Generic Fantasy Game X/Assets/Scripts/CameraController.cs
+++ Generic Fantasy Game X/Assets/Scripts/CameraController.cs
@@ -14,9 +14,8 @@ public class CameraController : MonoBehaviour {
 	bool following = false;
 	Vector3 lastPosition;
 
-	private float horizontalMove, verticalMove;
-
-	private Vector3 playerMoveInput, rotationTarget;
+	private Vector3 playerMoveInput, rotationTarget, mousePosition, mouseMovement;
+	private float playerScrollInput;
 	//camera raycast
 	private int layerMask = 1 << 9;
 
@@ -25,20 +24,19 @@ public class CameraController : MonoBehaviour {
 		cameraTarget = GameObject.FindGameObjectWithTag("Hero");
 		lastPosition = new Vector3(cameraTarget.transform.position.x, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
 		offset = new Vector3(cameraTarget.transform.position.x, cameraTarget.transform.position.y + offsetHeight, cameraTarget.transform.position.z - offsetDistance);
+
 	}
 
 	void Update()
 	{
-		horizontalMove = Input.GetAxis("Horizontal");
-		verticalMove = Input.GetAxis("Vertical");
-
 		playerMoveInput = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		mouseMovement = Vector3.zero;
 
-		Debug.DrawRay (this.transform.position, this.transform.forward*100, Color.green, 5f);
+		Debug.DrawRay (this.transform.position, this.transform.forward*40, Color.green, 5f);
 		if (!following && playerMoveInput.magnitude > 0f) {
 			RaycastHit hitInfo;
 			Ray cameraRay = new Ray (this.transform.position, this.transform.forward);
-			if (Physics.Raycast (cameraRay, out hitInfo, 200f, layerMask)) {
+			if (Physics.Raycast (cameraRay, out hitInfo, 60f, layerMask)) {
 				rotationTarget = hitInfo.point;
 				Debug.Log (hitInfo.point);
 			}
@@ -46,9 +44,11 @@ public class CameraController : MonoBehaviour {
 
 		//Rotation adjusted local movement
 		Vector3 adjustedVector;
-		adjustedVector = Quaternion.Euler (0, this.transform.eulerAngles.y, 0) * new Vector3 (horizontalMove, 0, verticalMove);
+		adjustedVector = Quaternion.Euler (0, this.transform.eulerAngles.y, 0) * playerMoveInput;
 		this.transform.Translate (adjustedVector * Time.deltaTime * 5, Space.World);
 
+		//Mouse position
+		EdgeMovement ();
 
 		if (Input.GetKey(KeyCode.Q)) {
 			rotate = -1;
@@ -62,11 +62,7 @@ public class CameraController : MonoBehaviour {
 			rotate = 0;
 		}
 
-		if (Input.GetAxis("Mouse ScrollWheel") != 0f) {
-			Debug.Log (Input.GetAxis ("Mouse ScrollWheel"));
-			this.transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * 200, Space.Self);
-			//this.transform.forward * Input.GetAxis("Mouse ScrollWheel");
-		}
+		CameraZoom ();
 
 
 //		if(Input.GetKey(KeyCode.F))
@@ -94,6 +90,49 @@ public class CameraController : MonoBehaviour {
 //			transform.position = lastPosition; 
 //		}
 //		transform.LookAt(cameraTarget.transform.position);
+	}
+
+	private void EdgeMovement() {
+		//mousePosition = Camera.main.WorldToViewportPoint (Input.mousePosition);
+		Debug.Log (Input.mousePosition);
+
+		//Debug.Log (mousePosition);
+		mousePosition = Input.mousePosition;
+		Vector3 adjustedVector;
+
+		if (mousePosition.x < 20) {
+			mouseMovement = mouseMovement + Vector3.left;
+		}
+		if (mousePosition.x > Screen.width - 20) {
+			mouseMovement = mouseMovement + Vector3.right;
+		}
+
+		if (mousePosition.y < 20) {
+			mouseMovement = mouseMovement + Vector3.back;
+		}
+		if (mousePosition.y > Screen.height - 20) {
+			mouseMovement = mouseMovement + Vector3.forward;
+		}
+
+		adjustedVector = Quaternion.Euler (0, this.transform.eulerAngles.y, 0) * mouseMovement;
+
+		this.transform.Translate (adjustedVector * Time.deltaTime * 5, Space.World);
+		//adjustedVector = Quaternion.Euler (0, this.transform.eulerAngles.y, 0) * playerMoveInput;
+	}
+
+	private void CameraZoom() {
+		if (Input.GetAxis("Mouse ScrollWheel") != 0f) {
+			Debug.Log (Input.GetAxis ("Mouse ScrollWheel"));
+			playerScrollInput = Input.GetAxis ("Mouse ScrollWheel");
+			if (this.transform.position.y <= 4 && playerScrollInput > 0) {
+				return;
+			}
+			if (this.transform.position.y >= 14 && playerScrollInput < 0) {
+				return;
+			}
+			this.transform.Translate (Vector3.forward * Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * 200, Space.Self);
+			//this.transform.forward * Input.GetAxis("Mouse ScrollWheel");
+		}
 	}
 
 	void LateUpdate()
