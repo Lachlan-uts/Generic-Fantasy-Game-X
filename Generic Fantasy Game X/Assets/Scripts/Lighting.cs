09e34e99ff2e13f2fa7110026634d372b568131f
Jacob Harris <jacob.o.harris@student.uts.edu.au>
2018-05-23 23:23:53 +1000

    further work on the lighting, added lights to more rooms and started to improve the lights scene so i can test and implement the functionality of the lights turning on when the player enters the room

--- Generic Fantasy Game X/Assets/Scripts/Lighting.cs
+++ Generic Fantasy Game X/Assets/Scripts/Lighting.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Lighting : MonoBehaviour {
 
 	public Light[] lights;
+	private bool on = true;
 	
 
 	// Use this for initialization
@@ -15,11 +16,18 @@ public class Lighting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space) && on){
 			for(int i = 0; i < lights.Length; i++){
 				lights[i].enabled = false;
+				on = false;
 			}
-		}
+		// if(Input.GetKeyDown(KeyCode.Space) && !on) {
+		// 	for(int i = 0; i < lights.Length; i++){
+		// 		lights[i].enabled = true;
+		// 		on = true;
+		// 	}
+		// }
 
+		}
 	}
 }
