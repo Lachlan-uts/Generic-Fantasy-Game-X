c21191a6941748f6a867935eaa5067f8dced6e73
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-05-26 16:26:43 +1000

    Floating UI
    Initial support for it working.

---
+++ Generic Fantasy Game X/Assets/FloatingUIScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingUIScript : MonoBehaviour {
+
+	private Vector3 camPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camPos = Camera.main.transform.position;
+//		transform.LookAt (new Vector3 (-camPos.x, transform.position.y, -camPos.z));
+		transform.LookAt (new Vector3 (transform.position.x, transform.position.y, transform.position.z-4));
+	}
+}
